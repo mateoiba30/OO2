948940c4561691d9a0473d002326c8726bf7a6ed
Mateo Ibanez <mateoiba30@gmail.com>
2024-03-19 19:42:28 -0300

    1er commit con ejs 1 y 2

---
+++ e1-twitter/src/main/java/ar/edu/unlp/info/oo2/e1_twitter/Posteo.java
@@ -0,0 +1,6 @@
+package ar.edu.unlp.info.oo2.e1_twitter;
+
+public abstract class Posteo {
+	
+	public abstract Posteo getOrigen();
+}
---
+++ e2-PPTijera/src/main/java/ar/edu/unlp/info/oo2/e2_PPTijera/Jugada.java
@@ -0,0 +1,16 @@
+package ar.edu.unlp.info.oo2.e2_PPTijera;
+
+public interface Jugada {
+	
+	public String contraPiedra();
+	
+	public String contraPapel();
+	
+	public String contraTijera();
+	
+	public String contraLagarto();
+	
+	public String contraSpock();
+
+
+}
